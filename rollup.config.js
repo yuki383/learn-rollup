f2130b26de522d1247a8ae40dd26cd25edc8d048
yuki kokubo <yuukikokubo.pg@gmail.com>
2023-03-05 16:22:12 +0900

    setup react

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,32 @@
 import replace from "@rollup/plugin-replace";
 import commonjs from "@rollup/plugin-commonjs";
 import terser from "@rollup/plugin-terser";
+import babel from "@rollup/plugin-babel";
+import resolve from "@rollup/plugin-node-resolve";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
 
 export default {
   input: "src/main.js",
   output: [
     {
       dir: "./dist",
+      format: "esm",
+      sourcemap: true,
     },
   ],
   plugins: [
+    peerDepsExternal(),
+    resolve({
+      browser: true,
+    }),
+    babel(),
+    commonjs({
+      include: ["node_modules/**"],
+    }),
     replace({
       preventAssignment: true,
+      "process.env.NODE_ENV": JSON.stringify("production"),
     }),
-    commonjs(),
     terser(),
   ],
 };
--- src/main.js
+++ src/main.js
@@ -1,3 +1,20 @@
-export default function () {
-  import("./foo.js").then(({ default: foo }) => console.log(foo));
+import { useState, useEffect } from "react";
+import { createRoot } from "react-dom/client";
+
+function Component() {
+  const [v, setV] = useState("");
+
+  useEffect(() => {
+    import("./foo.js").then(({ default: foo }) => setV(foo));
+  }, []);
+
+  if (v === "") {
+    return <div>Loading...</div>;
+  }
+
+  return <div>{v}</div>;
 }
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<Component />);
